b22a68e255b146331f28667c42036adc32b81cdf
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-04-03 16:22:50 +0000

    rails6-bookers2-debug-ruby3-main

--- rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @newbook = Book.new
     @user = @book.user
+    @post_comment = PostComment.new
   end
 
   def index
--- rails6-bookers2-debug-ruby3-main/db/schema.rb
+++ rails6-bookers2-debug-ruby3-main/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_03_085637) do
+ActiveRecord::Schema.define(version: 2023_04_03_154919) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2023_04_03_085637) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "book"
+    t.integer "book_id"
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "book_id"
   end
 
