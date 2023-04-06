2a9223ea7c7f17c4105524577dfa0122b551ed68
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-04-06 09:54:51 +0000

    rails6-bookers2-debug-ruby3-main

--- rails6-bookers2-debug-ruby3-main/config/routes.rb
+++ rails6-bookers2-debug-ruby3-main/config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
-  
+  get "search" => "searches#search"
+
   # ネストさせる
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
   resources :users do
   member do
    get :followings, :followers
