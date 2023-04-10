c9ca11d654fd4f7f9f539bf5537222caeadb3a73
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-04-10 13:06:34 +0000

    rails6-bookers2-debug-ruby3-main

--- rails6-bookers2-debug-ruby3-main/app/controllers/post_comments_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/post_comments_controller.rb
@@ -1,16 +1,19 @@
 class PostCommentsController < ApplicationController
-  
+
   def create
     book = Book.find(params[:book_id])
+    @comment = current_user.comments.new(comment_params)
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    render :post_comments #render先にjsファイルを指定
   end
-  
+
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    #renderしたときに@postのデータがないので@postを定義
+    @book = Book.find(params[:book_id])
+    render :post_comments  #render先にjsファイルを指定
   end
 
   private
